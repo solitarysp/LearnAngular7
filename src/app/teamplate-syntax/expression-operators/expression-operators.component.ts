60370905a9d4ef18211f92da369bc4b348408b30
thanhlv <lethanh9398@gmail.com>
2019-02-28 15:04:48 +0700

    teamplate-syntax

--- src/app/teamplate-syntax/expression-operators/expression-operators.component.ts
+++ src/app/teamplate-syntax/expression-operators/expression-operators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-expression-operators',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpressionOperatorsComponent implements OnInit {
 
-  constructor() { }
+  // ===
+  showPipe = false;
+  operatorHoiCham = false;
+  employeeNotInit: Employee;
+  employee: Employee;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.employee = new Employee();
+    this.employee.id = 1;
+  }
+
+}
+
+class Employee {
+  id;
+  name;
+  classz: Classz;
+}
+
+class Classz {
+  id;
 }
--- src/app/teamplate-syntax/teamplate-syntax.module.ts
+++ src/app/teamplate-syntax/teamplate-syntax.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { TeamplateSyntaxRoutingModule } from './teamplate-syntax-routing.module';
-import { TeamplateSyntaxComponent } from './teamplate-syntax.component';
-import { ReferenceVariablesComponent } from './reference-variables/reference-variables.component';
-import { ExpressionOperatorsComponent } from './expression-operators/expression-operators.component';
+import {TeamplateSyntaxRoutingModule} from './teamplate-syntax-routing.module';
+import {TeamplateSyntaxComponent} from './teamplate-syntax.component';
+import {ReferenceVariablesComponent} from './reference-variables/reference-variables.component';
+import {ExpressionOperatorsComponent} from './expression-operators/expression-operators.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [TeamplateSyntaxComponent, ReferenceVariablesComponent, ExpressionOperatorsComponent],
   imports: [
     CommonModule,
+    FormsModule,
     TeamplateSyntaxRoutingModule
   ]
 })
-export class TeamplateSyntaxModule { }
+export class TeamplateSyntaxModule {
+}

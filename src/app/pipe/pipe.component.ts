d4e75294e084b7475d0c036f2b2bfad1d67ee31f
thanhlv <lethanh9398@gmail.com>
2019-02-28 10:21:46 +0700

    pipe async một Observable

--- src/app/pipe/pipe.component.ts
+++ src/app/pipe/pipe.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {interval, Observable} from 'rxjs';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-pipe',
@@ -9,11 +11,18 @@ export class PipeComponent implements OnInit {
   birthday = new Date();
   formatBirthday;
   listNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  message$: Observable<string>;
+  private messages = [
+    'You are my hero!',
+    'You are the best hero!',
+    'Will you be my hero?'
+  ];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.resend();
   }
 
   addNumber() {
@@ -22,5 +31,12 @@ export class PipeComponent implements OnInit {
 
   addNumberNewArray() {
     this.listNumber = this.listNumber.concat(new Date().getMilliseconds());
+  }
+
+  resend() {
+    this.message$ = interval(500).pipe(
+      map(i => this.messages[i]),
+      take(this.messages.length)
+    );
   }
 }

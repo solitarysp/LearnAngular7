9a1a3cda29d97079f86b2dd8ad6eec319a4fe679
le.van.thanh <lethanh9398@gmail.com>
2019-04-18 16:43:32 +0700

    angular-animations

--- src/app/angular-animations/animations-transitions-and-triggers/animations-transitions-and-triggers.component.ts
+++ src/app/angular-animations/animations-transitions-and-triggers/animations-transitions-and-triggers.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-animations-transitions-and-triggers',
   templateUrl: './animations-transitions-and-triggers.component.html',
-  styleUrls: ['./animations-transitions-and-triggers.component.scss']
+  styleUrls: ['./animations-transitions-and-triggers.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => *', [
+        animate('1s')
+      ]),
+      transition('* => open', [
+        animate('0.5s')
+      ]),
+      transition('* => *', [
+        animate('1s')
+      ]),
+    ]), trigger('voidandasterisk', [
+      // ...
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate('1s')
+      ]),
+      transition('* => void', [
+        animate('0.5s', style({transform: 'translateX(-100%)'}))
+      ]),
+    ]),
+
+  ]
 })
 export class AnimationsTransitionsAndTriggersComponent implements OnInit {
 
-  constructor() { }
+  isOpen = true;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
